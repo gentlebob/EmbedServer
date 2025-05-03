a1fe8fe1997d783bbb74dc9908ca9bd83d721242
bob <>
2025-05-02 23:08:56 -0700

    initial embed/redirect implementation

--- src/app.py
+++ src/app.py
@@ -1,6 +1,55 @@
 from flask import Flask
+import yt_dlp
+
 app = Flask(__name__)
+
+ydl = yt_dlp.YoutubeDL({
+    'skip_download': True,
+    'dump_single_json': True, # Get the info as a single JSON object
+    'extract_flat': True,    # Extract info without resolving formats (faster for just info)
+    'verbose': False,        # Set to True to see yt-dlp's internal output
+    'logger': None,          # Optional: provide a custom logger if needed
+})
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return 'buh'
+
+@app.route('/t/<path:id>')
+def redirect_to_target(id):
+    """
+    Returns an HTML page with a script that redirects to the path
+    provided in the URL after /t/.
+    """
+    global ydl
+
+    try:
+        vinfo = ydl.extract_info(f'https://clips.twitch.tv/{id}', download=False)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+    
+    video_format = vinfo["formats"][max(3, len(vinfo["formats"]) - 1)]
+
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Website Name</title>
+        <meta content="{vinfo["channel"]}" property="og:title" />
+        <meta content="{vinfo["title"]}" property="og:description" />
+        <meta content="https://clips.twitch.tv/{id}" property="og:url" />
+        <meta content="{video_format["url"]}" property="og:video" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
+    </head>
+    <body>
+        <script>
+            // Redirect using JavaScript to the target URL
+            window.location.replace('https://clips.twitch.tv/{id}');
+        </script>
+    </body>
+    </html>
+    """
+    # Return the HTML content with the appropriate MIME type
+    return html_content
+
